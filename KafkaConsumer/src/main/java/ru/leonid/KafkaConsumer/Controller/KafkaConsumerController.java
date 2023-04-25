1e20bb10561c40d24310b53778bf44c722f8d273
Leonid <ewro01@yandex.ru>
2023-04-25 16:44:30 +0300

    Thumeleaf with WebFlux

--- KafkaConsumer/src/main/java/ru/leonid/KafkaConsumer/Controller/KafkaConsumerController.java
+++ KafkaConsumer/src/main/java/ru/leonid/KafkaConsumer/Controller/KafkaConsumerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.spring6.context.webflux.IReactiveDataDriverContextVariable;
 import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
@@ -14,6 +15,7 @@ public class KafkaConsumerController {
     @Autowired
     ConsumerService consumerService;
     @GetMapping("/")
+    @ResponseBody
     public Flux<String> getRecords(){
         return consumerService.getCurrentMessages().map(record -> record + "<br>");
     }
